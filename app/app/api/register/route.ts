49c85ae4e8ca422ada7c57c91af7c5892c116699
RomaSorokivskiy <sorokivroma@gmail.com>
2023-09-17 14:32:35 +0300

    Big commit

---
+++ app/app/api/register/route.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+
+import prismaDb from "@/app/libs/prisma.db";
+import {NextResponse} from "next/server"
+
+export async function POST(
+    request: Request
+) {
+    try {
+        const body = await request.json()
+        const {
+            email,
+            name,
+            password
+        } = body
+        if(!email || !name || password) {
+            return new NextResponse('Missing Info', {status: 400})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,12)
+
+        const user = await prismaDb.user.create({
+            data:{
+                email,
+                name,
+                hashedPassword
+            }
+        })
+
+        return NextResponse.json(user)
+    } catch (error: any) {
+        console.log(error, "REGISTRATION_ERROR");
+        return new NextResponse("Internal ERROR", {status: 500})
+    }
+}
